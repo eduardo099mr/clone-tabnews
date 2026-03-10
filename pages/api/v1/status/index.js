0c7502585d98f44cb3b1930ed1fee08faea38361
Eduardo Marinho <eduardo099m@gmail.com>
2026-03-10 16:00:23 -0300

    Adiciona os arquivos `compose.yaml`, `.env` e `database.js`.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database";
+
+async function status(request, response) {
   response.status(200).json({ chave: "Resposta da requisição" });
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
 }
 
 export default status;
