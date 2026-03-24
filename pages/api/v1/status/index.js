6cd1e9f7881debbefdb4757b9a016771fca6d5b2
Eduardo Marinho <eduardo099m@gmail.com>
2026-03-24 13:33:16 -0300

    Finaliza o endpoint `/status`.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  response.status(200).json({ chave: "Resposta da requisição" });
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const result_databasequery = await database.query("SHOW server_version;");
+  const db_version = result_databasequery.rows[0].server_version;
+  console.log("VERSION: " + db_version);
+
+  const maxConnectiosresult = await database.query("SHOW max_connections;");
+  const maxConnectiosValue = parseInt(
+    maxConnectiosresult.rows[0].max_connections,
+  );
+  console.log("MAX_CONNECTIONS: " + maxConnectiosValue);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WhERE datname = $1;",
+    values: [databaseName],
+  });
+  console.log("OPENED_CONNECTIONS: " + openedConnectionsResult.rows[0].count);
+  const openedConnectionsValue = openedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      version: db_version,
+      max_connections: maxConnectiosValue,
+      opened_connections: openedConnectionsValue,
+    },
+  });
 }
 
 export default status;
--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -1,10 +1,30 @@
 test("GET deveria retornar 200", async () => {
-  const resolve = await fetch("http://localhost:3000/api/v1/status/");
-  console.log(resolve.status);
-  if (resolve.status == 200) {
+  const response = await fetch("http://localhost:3000/api/v1/status/");
+  console.log(response.status);
+  if (response.status == 200) {
     console.log("Conexão Estabelecida");
   } else {
     console.log("Falha na conexão");
   }
-  expect(resolve.status).toBe(200);
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  console.log(parsedUpdatedAt);
+  console.log("UPDATED_AT: " + responseBody.updated_at);
+  expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
+
+  console.log("VERSION: " + responseBody.dependencies.version);
+  expect(responseBody.dependencies.version).toEqual("16.0");
+
+  console.log("MAX_CONNECTIONS: " + responseBody.dependencies.max_connections);
+  expect(responseBody.dependencies.max_connections).toEqual(100);
+
+  console.log(
+    "OPENED_CONNECTIONS: " + responseBody.dependencies.opened_connections,
+  );
+  expect(responseBody.dependencies.opened_connections).toEqual(1);
 });
