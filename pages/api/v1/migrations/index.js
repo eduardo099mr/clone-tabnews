d0049262e7aab04708411d30287eef4cee7a10c4
Eduardo Marinho <eduardo099m@gmail.com>
2026-04-22 14:26:41 -0300

    Aprimora o tratamento de erro de mmetodo invalido no endpoint `/migrations`.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -5,7 +5,9 @@ import database from "infra/database";
 export default async function migrations(request, response) {
   const allowedMethods = ["GET", "POST"];
   if (!allowedMethods.includes(request.method)) {
-    return response.status(405).end();
+    return response.status(405).json({
+      error: `${request.method} não é um método valido!`,
+    });
   }
 
   let dbClient;
